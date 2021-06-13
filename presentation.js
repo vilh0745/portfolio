bcf7aa3dad1c8f920e3581ec28d683d33e3b9ae9
Vilhelm Saxild <vilh0745@stud.kea.dk>
2021-06-13 17:24:07 +0200

    portfolio

---
+++ presentation.js
@@ -0,0 +1,17 @@
+function on() {
+  document.getElementById("theme").classList.add("light_dark");
+  document.getElementById("image").classList.add("no_invert");
+  document.getElementById("image2").classList.add("no_invert");
+  document.getElementById("image3").classList.add("no_invert");
+  document.getElementById("image4").classList.add("no_invert");
+  document.getElementById("image5").classList.add("no_invert");
+}
+
+function off() {
+  document.getElementById("theme").classList.remove("light_dark");
+  document.getElementById("image").classList.remove("no_invert");
+  document.getElementById("image2").classList.remove("no_invert");
+  document.getElementById("image3").classList.remove("no_invert");
+  document.getElementById("image4").classList.remove("no_invert");
+  document.getElementById("image5").classList.remove("no_invert");
+}
