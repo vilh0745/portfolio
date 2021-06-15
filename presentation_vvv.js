7d2fb923275b75889a5662dc50790bc7fa353256
Vilhelm Saxild <vilh0745@stud.kea.dk>
2021-06-15 12:33:57 +0200

    tekster til alle forløb

--- presentation_vvv.js
+++ presentation_vvv.js
@@ -2,6 +2,7 @@ function on() {
   document.getElementById("theme").classList.add("light_dark");
   document.getElementById("image").classList.add("no_invert");
   document.getElementById("image2").classList.add("no_invert");
+  document.getElementById("image3").classList.add("no_invert");
   document.getElementById("image4").classList.add("no_invert");
   document.getElementById("image5").classList.add("no_invert");
 }
@@ -10,6 +11,7 @@ function off() {
   document.getElementById("theme").classList.remove("light_dark");
   document.getElementById("image").classList.remove("no_invert");
   document.getElementById("image2").classList.remove("no_invert");
+  document.getElementById("image3").classList.remove("no_invert");
   document.getElementById("image4").classList.remove("no_invert");
   document.getElementById("image5").classList.remove("no_invert");
 }
